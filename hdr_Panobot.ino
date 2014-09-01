e14ce2dedcb0ef957c33ac34a23a5bbd0884d024
chamoladri <mauropetrelli@gmail.com>
2014-09-01 00:51:06 -0300

    funciones

--- hdr_Panobot.ino
+++ hdr_Panobot.ino
@@ -4,21 +4,65 @@
   Turns on an LED on for one second, then off for one second, repeatedly.
 */
 
+int photoPin = 10;
+int shutterPin = 11;
+int contador = 0;
+
 void setup()
 {
 	Serial.begin(9600);
 
 	// initialize the digital pin as an output.
 	// Pin 13 has an LED connected on most Arduino boards:
-	pinMode(13, OUTPUT);
+	pinMode(photoPin,OUTPUT);
+	pinMode(shutterPin,OUTPUT);
 }
 
-void loop()
-{
-	Serial.println("Hello world!");
+void exposureTime(int time){
 
-	delay(1000);              // wait for a second
-	digitalWrite(13, HIGH);   // set the LED on
-	delay(1000);              // wait for a second
-	digitalWrite(13, LOW);    // set the LED off
+digitalWrite(shutterPin,HIGH);
+delay(time);
+digitalWrite(shutterPin,LOW);
+Serial.println("Foto");
+
 }
+
+void singleShoot(){
+
+digitalWrite(photoPin,HIGH);
+delay(50);
+digitalWrite(photoPin,LOW);
+delay(20);
+}
+
+///////////////////////////////////////
+
+void hdrShoot(int fstop){
+
+    for(int x =0;x <fstop;++x){
+
+      exposureTime(200*x);
+      singleShoot();
+      Serial.println("loop");
+    }
+}
+
+////////////////////////////////////////////////
+
+void loop(){
+
+
+    while (contador < 5 ) {
+        //singleShoot();
+        hdrShoot(5);
+        delay(100);
+        ++contador;
+      }
+
+}
+
+
+
+
+
+
